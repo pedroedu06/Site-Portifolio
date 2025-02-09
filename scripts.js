a321fd63b2372cba3f4ca5fb94a861056c487ee3
pedroedu06 <pedro.nascimento182501@gmail.com>
2025-02-09 18:28:53 -0300

    teste de download

--- scripts.js
+++ scripts.js
@@ -56,9 +56,17 @@ links.forEach(link =>{
     
 })
 
-//aba de demo e git
+//donwloads 
+
+function downloadCurriculo(url, filename) {
+    const link = document.createElement('a');
+    link.href = URL;
+    link.download = filename;
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
+}
 
 
 
 
-//aba de precos
